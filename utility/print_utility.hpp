95a1554564be13209d8750e5557ae46e3587397e
Martin <martin-z@sjtu.edu.cn>
2025-04-04 17:58:00 +0800

    update

---
+++ utility/print_utility.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../trie/src.hpp"
+
+// Feel free to design your own print function for testing / debugging purpose
+void print_trie(std::shared_ptr<sjtu::TrieNode> trienode,
+                std::vector<std::string> path);
